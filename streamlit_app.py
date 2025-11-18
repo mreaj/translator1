c883483dc2ff166b4b130c36090f1a52ab8dad8d
mreaj <mreaj@vestas.com>
2025-11-18 21:18:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,6 @@ st.set_page_config(page_title="Streamlit Translator", layout="centered")
 st.title("🌍 Language Translator")
 st.write("Translate text instantly between more than 100 languages.")
 
-translator = Translator()
-
-# Input text
-text = st.text_area("Enter text to translate:")
-
-# Language options
 languages = {
     "English": "en",
     "Tamil": "ta",
@@ -23,6 +17,8 @@ languages = {
     "Japanese": "ja",
 }
 
+text = st.text_area("Enter text to translate:")
+
 source = st.selectbox("From", list(languages.keys()), index=0)
 target = st.selectbox("To", list(languages.keys()), index=1)
 
@@ -32,6 +28,7 @@ if st.button("Translate"):
             source=languages[source],
             target=languages[target]
         ).translate(text)
+        
         st.success(translated)
     except Exception as e:
-        st.error("Translation failed. Try later.")
+        st.error("Translation failed.")
